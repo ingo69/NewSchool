68afe2c01c2c38c341ce74dcca5b62cfeb7ad413
Ingo Ngoyama <ingo.srg@gmail.com>
2021-10-06 22:12:56 -0700

    commit IngoN branch

--- NewSchool/NewSchool/Controllers/ViewController.swift
+++ NewSchool/NewSchool/Controllers/ViewController.swift
@@ -14,6 +14,8 @@ import AVFoundation
 class ViewController: UIViewController {
 
     
+    
+    
     @IBOutlet weak var sceneView: ARSCNView!
     let configuration = ARWorldTrackingConfiguration()
     
